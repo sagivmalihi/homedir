3ea4a0b856bfca33e88c8c409c430af4955686e4
Sagiv Malihi <sagivmalihi@gmail.com>
2010-07-27 15:44:38 +0300

    better python startup script

--- work/python/startup.py
+++ work/python/startup.py
@@ -1,6 +1,25 @@
+import atexit
+import os
+import readline
+import rlcompleter
 from tools import *
+
+# aliases
 ex = execfile
 
-import rlcompleter
+# tab-completion
 rlcompleter.readline.parse_and_bind('tab: complete')
+
+# history saving
+historyPath = os.path.expanduser("~/.pyhistory")
+
+def save_history(historyPath=historyPath):
+    import readline
+    readline.write_history_file(historyPath)
+
+atexit.register(save_history)
+
+if os.path.exists(historyPath):
+    readline.read_history_file(historyPath)
+
 

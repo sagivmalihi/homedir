0afa5205ba3992acd9542cb3d2ca4e238c7fca69
Sagiv Malihi <sagivmalihi@gmail.com>
2011-06-02 19:05:44 +0300

    improvements to loggeddict

--- work/python/loggeddict.py
+++ work/python/loggeddict.py
@@ -1,16 +1,19 @@
-import traceback
+import sys
 
 class LoggedDict(dict):
-    def __init__(self, *kargs, **kwargs):
-        super(LoggedDict, self).__init__(*kargs, **kwargs)
-    def __getitem__(self, *kargs, **kwargs):
-        print "getitem"
-        return super(LoggedDict, self).__getitem__(*kargs, **kwargs)    
-    def __setitem__(self, *kargs, **kwargs):
-        print "setitem"
-        return super(LoggedDict, self).__setitem__(*kargs, **kwargs)
-    def __delitem__(self, *kargs, **kwargs):
-        print "delitem"
-        traceback.print_stack()
-        return super(LoggedDict, self).__delitem__(*kargs, **kwargs)
+    def __init__(self, *args, **kwargs):
+        self.file = kwargs.pop('file', sys.stdout)
+        super(LoggedDict, self).__init__(*args, **kwargs)
+    def __getitem__(self, *args, **kwargs):
+        rtn = super(LoggedDict, self).__getitem__(*args, **kwargs)    
+        with open(self.file, 'a') as f:
+            f.write("getitem %s %s -> %s\n" % (args, kwargs, rtn))
+    def __setitem__(self, *args, **kwargs):
+        with open(self.file, 'a') as f:
+            f.write("setitem %s %s\n" % (args, kwargs))
+        return super(LoggedDict, self).__setitem__(*args, **kwargs)
+    def __delitem__(self, *args, **kwargs):
+        with open(self.file, 'a') as f:
+            f.write("delitem %s %s\n" % (args, kwargs))
+        return super(LoggedDict, self).__delitem__(*args, **kwargs)
 
